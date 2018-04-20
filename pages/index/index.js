cba3a803a1a0afe211e9c60e96b0dbf596068805
baopan <baopan@gaosiedu.com>
2018-04-20 17:33:39 +0800

    sss

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,29 @@ Page({
   data: {
     text: "大乐透历史中奖号码",
     daletouList:[
-      {"id":1,"reds":[1,2,3,4,5],"blus":[6,7]},
-      {"id": 2, "reds": [1, 2, 3, 4, 5], "blus": [6, 7] },
-      { "id": 3, "reds": [1, 2, 3, 4, 5], "blus": [6, 7] },
-      { "id": 4, "reds": [1, 2, 3, 4, 5], "blus": [6, 7] }
     ]
   },
 
-  onLaunch: function () {
-    console.log('App Launchsssssssssssssssssssss')
-  }
-
-
+  onLoad: function () {
+    console.log('ssssssssssssssApp Launchsssssssssssssssssssss')
+    this.RequestData()
+  },
+RequestData: function () { 
+  var that = this;
+  wx.request({
+    url: 'http://currentbp.com/daletou/list',
+    header: {
+      'content-type': 'application/json'
+    },
+    success: function (res) {
+      console.log(res.data.body)
+      if (1 == res.data.status) {
+        that.setData({ daletouList: res.data.body })
+      } else {
+        console.log("=========   error       ================")
+      }
+    }
+  })
+}
 
 })
